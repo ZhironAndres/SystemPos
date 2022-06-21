9170140c7b1fe2df1082a1117a622a55f4fa518f
ZhironAndres <morales.a.h.98@gmail.com>
2022-06-21 11:06:53 -0500

    Agregando cambios

---
+++ php/paginaPrincipalEmpresa.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SystemPos</title>
+    <link rel="stylesheet" href="../CSS/all.min.css">
+    <link rel="stylesheet" href="../CSS/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/e9c2883c48.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="shortcut icon" href="../IMG/logo.jpeg">
+    <link rel="stylesheet" href="../CSS/profileCompany.css">
+</head>
+<body>
+<div class="loader">
+        <img src="../IMG/loader.gif" alt="">
+    </div>
+    <header>
+        <nav class="nav-tag">
+            <div class="nav-logo">
+                <img class="logo" src="../IMG/logo.jpeg" alt="Logo"></li>
+            </div>
+            <div class="container-list">
+                <ul class="nav-menu">
+                    <li class="nav-item"><a class="nav-link" href="#">Perfil</a></li>                    
+                    <li class="nav-item"><a class="nav-link" href="talento.php">Buscar Talento</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#Contacto">Cerrar Sesion</a></li>
+                </ul>
+            </div>
+            <div class="hamburger">
+               <span class="bar"></span> 
+               <span class="bar"></span> 
+               <span class="bar"></span>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="display">
+            <section class="profileDisplay">
+                <div class="cardProfile">
+                    <div class="imageContainer">
+                        <div class="imageBox">
+                            <img src="../IMG/logoEmpresa.png" alt="Imagen correspondiente a la Empresa">
+                        </div>
+                    </div>
+                    <div class="bienvenidaUsuario">
+                    <div class="nombre"><h4>ECOPETROL</h4></div>  
+                    </div>
+                    <div class="littleDescription"> 
+                    <h4>Descripcion</h4>                    
+                    <div>                   
+                        <p>Somos una empresa  de actividades comerciales o industriales correspondientes o relacionadas con la exploración, explotación, refinación, transporte, almacenamiento, distribución y comercialización de hidrocarburos</p>
+                        </div>                        
+                    </div>
+                    <div class="datosPersonales">
+                    <div class="info">
+                        <h4>Datos de la Empresa</h4>
+                       <div>    
+                        <div class="datos"> <p>Ciudad de ubicacion: <span>Cartagena de Indias</span> </p></div>
+                        <div class="datos"> <p>Departamento: <span>Bolivar</span></p> </div>
+                        <div class="datos"> <p>Fecha de creacion:    <span>1998 - 06 -20</span></p> </div>
+                        
+                       <div class="datos"> <p>No. Nit:  <span>47896560212</span></p> </div>                     
+                    <div class="datos"> <p>Telefono:  <span>301145454</span></p> </div>
+                    <div class="datos"> <p>Correo:   <span>ml@gmail.com</span></p> </div>                    
+                    <div class="datos"> <p>Direccion:   <span>Nelson mandela sector tal</span></p> </div> 
+                       </div>     
+                    </div>
+                    </div>
+                    <div class="botonActualizar">
+                        <button><a href="#">Actualizar datos </a> <i class="fa-solid fa-pen-to-square"></i></button>
+                </div>
+            </div>
+            </section>
+            <!-- <section class="infoDisplay">
+               <div class="information">
+                    <div class="titulo"> <h5>Experiencia laboral <i class="fa-solid fa-briefcase"></i></h5></div>
+                    <div class="campos"><p>Nombre de la empresa: <span>Ecopetrol</span></p></div>
+                    <div class="campos"><p>Fecha de inicio: <span>Ecopetrol</span></p></div>
+                    <div class="campos"><p>Nombre de finalizacion: <span>Ecopetrol</span></p></div>
+                    <div class= "certify">                        
+                        <p>Certificados:</p>
+                    </div>
+                    <div class= "files">
+                        <p>Agregar certificado</p>
+                        <input type="file" name="" id="">
+                    </div>
+                </div>
+                <div class="information">
+                    <div class="titulo"> <h5>Estudios <i class="fa-solid fa-graduation-cap"></i></h5></div>
+                    <div class="campos"><p>Nombre de la institucion: <span>Ecopetrol</span></p></div>
+                    <div class="campos"><p>Fecha de inicio: <span>01-05-2002</span></p></div>
+                    <div class="campos"><p>Fecha de finalizacion: <span>08-12-2010</span></p></div>
+                    <div class= "certify">                        
+                        <p>Certificados:</p>
+                    </div>
+                    <div class= "files">
+                        <p>Agregar certificado</p>
+                        <input type="file" name="" id="">
+                    </div>
+                </div>
+                <div class="information">
+                    <div class="titulo"> <h5>Habilidades y Herramientas <i class="fa-solid fa-screwdriver-wrench"></i></h5></div>
+                    <div class="campos"><p>Cargo: <span>Desarrollador Movil</span></p></div>
+                    <div class="campos"><p>Lenguajes de Programacion: <span>Flutter, Dart</span></p></div>
+                    <div class="campos"><p>Otros: <span>Git, Github</span></p></div>
+                    <div class= "certify">                        
+                        <p>Certificados:</p>
+                    </div>
+                    <div class= "files">
+                        <p>Agregar certificado</p>
+                        <input type="file" name="" id="">
+                    </div>
+                </div>
+                <div class="information">
+                <div class="buttonActualizar">
+                        <button><a href="#">Actualizar datos </a> <i class="fa-solid fa-pen-to-square"></i></button>
+                </div>
+                </div>
+            </section>-->
+        </section>
+    </main>
+    <footer></footer>
+    <script src="../JS/loader2.js"></script>
+</body>
+</html>
---
+++ php/talento.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SystemPos</title>
+    <link rel="stylesheet" href="../CSS/all.min.css">
+    <link rel="stylesheet" href="../CSS/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/e9c2883c48.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="shortcut icon" href="../IMG/logo.jpeg">
+    <link rel="stylesheet" href="../CSS/talento.css">
+</head>
+<body>
+<div class="loader">
+        <img src="../IMG/loader.gif" alt="">
+    </div>
+    <header>
+        <nav class="nav-tag">
+            <div class="nav-logo">
+                <img class="logo" src="../IMG/logo.jpeg" alt="Logo"></li>
+            </div>
+            <div class="container-list">
+                <ul class="nav-menu">
+                    <li class="nav-item"><a class="nav-link" href="paginaPrincipalEmpresa.php">Perfil</a></li>                    
+                    <li class="nav-item"><a class="nav-link" href="php/iniciarSesion.php">Buscar Talento</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#Contacto">Cerrar Sesion</a></li>
+                </ul>
+            </div>
+            <div class="hamburger">
+               <span class="bar"></span> 
+               <span class="bar"></span> 
+               <span class="bar"></span>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="display">
+            
+        </section>
+    </main>
+    <footer></footer>
+    <script src="../JS/loader2.js"></script>
+</body>
+</html>
